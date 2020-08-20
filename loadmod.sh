647bc4936dd0958de48aa588170d157238ff32c4
Lana Abadie <lana.abadie@iter.org>
2020-08-20 10:50:26 +0200

    put strict dependency

--- loadmod.sh
+++ loadmod.sh
@@ -1,4 +1,7 @@
-module load IMAS
+#module load IMAS
+module load Python/3.6.4-intel-2018a
+module load matplotlib/2.1.2-intel-2018a-Python-3.6.4
+module load libMemcached/1.0.18-GCCcore-6.4.0
 module load PyQt5/5.9.2-intel-2018a-Python-3.6.4
 module load SWIG/3.0.12-intel-2018a-Python-3.6.4
 module load CMake/3.12.1
