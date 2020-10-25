907387a8be6a2fec712629f470e850207b1dab0a
pmazur <piotr.mazur@vmlyr.com>
2020-10-25 18:59:40 +0100

    Updated readme and loadmod

--- loadmod.sh
+++ loadmod.sh
@@ -3,14 +3,17 @@ module load Python/3.8.2-GCCcore-9.3.0
 module load PyQt5/5.13.2-GCCcore-9.3.0-Python-3.8.2
 module load libMemcached/1.0.18-GCCcore-9.3.0
 
-#This app requies gnuplot binary with qt terminal support
+# Support for gnuplot QT widget requires gnuplot binary with Qt support and widget lbrary
 GNUPLOT_PATH=/home/ITER/mazurp/gnuplot/bin/bin
 GNUPLOTWIDGET_PATH=/home/ITER/mazurp/iplotlib-gnuplot/qt/gnuplotwidget/gnuplotwidget/lib
+# Support for UDA
 UDA_PYTHON_PATH=/home/ITER/mazurp/udaclientPy38GCC9/udahpcPy38/lib/
+# The plot library itself if not already in PYTHONPATH
+PLOTLIBRARY_PATH=/home/ITER/mazurp/iterplot
 
 OPENSSL_PATH=/work/imas/opt/EasyBuild/software/OpenSSL/1.0.1f-goolf-1.5.16/lib/
 
-export UDA_HOST=10.153.0.204
+export UDA_HOST=io-ls-udafe01.iter.org
 export PATH=${GNUPLOT_PATH}:${PATH}
 export LD_LIBRARY_PATH=${OPENSSL_PATH}:${GNUPLOTWIDGET_PATH}:${UDA_PYTHON_PATH}:${LD_LIBRARY_PATH}
-export PYTHONPATH=${UDA_PYTHON_PATH}:${PYTHONPATH}
+export PYTHONPATH=${UDA_PYTHON_PATH}:${PYTHONPATH}${PLOTLIBRARY_PATH}
