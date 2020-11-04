5a09de600e53f1149d1e2b214a265f970679743d
root <root@4501WS-CC-0013.codac.iter.org>
2020-11-04 09:04:59 +0000

    modify the main to add choice between gnuplot and matplotlib and also for the date shows automatically last week of data

--- main.py
+++ main.py
@@ -16,12 +16,21 @@ from qt.gnuplot.QtGnuplotMultiwidgetCanvas import QtGnuplotMultiwidgetCanvas
 from qt.matplotlib.QtMatplotlibCanvas2 import QtMatplotlibCanvas2
 
 from widgets.Uda import MainCanvas, MainMenu, PlotToolbar, UDARangeSelector, UDAVariablesTable
-
+from datetime import timedelta,datetime
 
 if __name__ == '__main__':
 
     da = DataAccess()
-    da.udahost = os.environ.get('UDA_HOST') or "10.153.0.204"
+    da.udahost = os.environ.get('UDA_HOST') or "io-ls-udafe01.iter.org"
+    canvasImpl="MATPLOTLIB"
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'MATPLOTLIB' or sys.argv[1] == 'GNUPLOT':
+            canvasImpl = sys.argv[1]
+    
+    
+    currTime = datetime.now().isoformat(timespec='seconds')
+    currTimeDelta = datetime.now() - timedelta(days=7)
+
 
     app = QApplication(sys.argv)
 
@@ -32,21 +41,21 @@ if __name__ == '__main__':
             ["UTIL-PHV-P400-BAY3:41PPAC_TC3000-IT01", "1.1.1"]
             ,["UTIL-PHV-P400-BAY3:41PPAC_TC3000-IT02", "1.1.2"]
         ],
-        "range": {"mode": UDARangeSelector.TIME_RANGE, "value": ["2020-09-18T08:00:00", "2020-09-30T08:00:00"]}
+        "range": {"mode": UDARangeSelector.TIME_RANGE, "value": [currTimeDelta.isoformat(timespec='seconds'), currTime]}
     }
 
     pan_model = {
         "table": [
             ["CWS-SCSU-HR00:AISPARE-2169-XI", "1.1.1"]
         ],
-        "range": {"mode": UDARangeSelector.TIME_RANGE, "value": ["2020-10-21T14:30:52.195Z", "2020-10-21T14:50:55.195Z"]}
+        "range": {"mode": UDARangeSelector.TIME_RANGE, "value": [currTimeDelta.isoformat(timespec='seconds'), currTime]}
     }
 
     pan_model2 = {
         "table": [
             ["UTIL-SYSM-COM-4503-UT:SRV3601-NRBPS", "1.1.1"]
         ],
-        "range": {"mode": UDARangeSelector.TIME_RANGE, "value": ["2020-10-14T14:30:52.195Z", "2020-10-21T14:50:55.195Z"]}
+        "range": {"mode": UDARangeSelector.TIME_RANGE, "value": [currTimeDelta.isoformat(timespec='seconds'), currTime]}
     }
 
     empty_model = {}
@@ -71,9 +80,10 @@ if __name__ == '__main__':
     left_column.layout().addWidget(range_selector)
     left_column.layout().addWidget(variables_table)
     left_column.layout().addWidget(draw_button)
-
-    right_column = MainCanvas(plot_canvas=QtMatplotlibCanvas2(tight_layout=False))
-    # right_column = MainCanvas(plot_canvas=QtGnuplotMultiwidgetCanvas())
+    if (canvasImpl=="MATPLOTLIB"):	
+        right_column = MainCanvas(plot_canvas=QtMatplotlibCanvas2(tight_layout=False))
+    else:
+        right_column = MainCanvas(plot_canvas=QtGnuplotMultiwidgetCanvas())
 
     central_widget = QSplitter()
     central_widget.setContentsMargins(10, 0, 10, 10)
