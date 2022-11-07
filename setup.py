d55e4355b70059725a33fc3226c1dd51d2d93774
Sawantdesai Prasad parshuram EXT <Prasadparshuram.Sawantdesai@iter.org>
2022-11-07 11:47:37 +0100

    IDV-393 corrected dependencies and allowed to have dirty version of dependencies

--- setup.py
+++ setup.py
@@ -35,9 +35,10 @@ setuptools.setup(
     packages=setuptools.find_packages(where="."),
     python_requires=">=3.8",
     install_requires=[
-        "iplotlib >= 0.5.0",
-        "iplotDataAccess >= 0.4.0",
-        "iplotProcessing >= 0.4.0",
+        "iplotlogging>=0.2.1*",
+        "iplotlib >= 0.5.0*",
+        "iplotDataAccess >= 0.4.0*",
+        "iplotProcessing >= 0.4.0*",
         "psutil >= 5.8.0",
     ],
     entry_points={
