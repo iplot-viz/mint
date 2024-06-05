4de10941f3d410fd61dfde39e049d713e8c22b4c
pmartin <pmartin@minsait.com>
2024-06-05 17:26:21 +0200

    IDV-535 Fix command-lines to have one dash if it has one letter and 2 dashes if it has more than one

--- mint/app/createApp.py
+++ mint/app/createApp.py
@@ -14,15 +14,15 @@ def create_app(argv=None) -> (QApplication, Namespace):
     parser.add_argument('--impl', metavar='canvas_impl',
                         help='Use canvas implementation (matplotlib/vtk...)', default="matplotlib")
     parser.add_argument('--use-fallback-samples', dest='use_fallback_samples', action='store_true', default=False)
-    parser.add_argument('--b', dest='blueprint_file', metavar='blueprint_file',
+    parser.add_argument('-b', dest='blueprint_file', metavar='blueprint_file',
                         help='Load blueprint from .json file', default=None)
-    parser.add_argument('--d', dest='scsv_file', metavar='scsv_file',
+    parser.add_argument('-d', dest='scsv_file', metavar='scsv_file',
                         help='Load variables table from file')
     parser.add_argument('--ld', dest='last_dump', action='store_true', default=False,
                         help='Load variables table from last dump file')
-    parser.add_argument('--w', dest='json_file', metavar='json_file',
+    parser.add_argument('-w', dest='json_file', metavar='json_file',
                         help='Load a workspace from json file')
-    parser.add_argument('--e', dest='image_file', metavar='image_file',
+    parser.add_argument('-e', dest='image_file', metavar='image_file',
                         help='Load canvas from JSON and save to file (PNG/SVG/PDF...)')
     parser.add_argument('--ew', dest='export_width', metavar='export_width',
                         type=int, default=1920, help='Exported image width')
