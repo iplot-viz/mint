7225ff2821af2f15bc1b344d8faea23ea7acb9e6
Pablo Martin <pmartin@minsait.com>
2024-08-20 10:01:26 +0200

    IDV-575 Apply PEP-8 and fix mismatch import with setLogger

--- mint/__main__.py
+++ mint/__main__.py
@@ -4,28 +4,29 @@
 import sys
 from mint.app import create_app, dirs, run_app
 import os
-import subprocess
-import iplotLogging.setupLogger as SetupLog
- 
-logger = setupLog.get_logger(__name__)
-def loadEnv(fpath):
-    with open(fpath,'r') as f:
-        line=f.readline()
+from iplotLogging import setupLogger
+
+logger = setupLogger.get_logger(__name__)
+
+
+def load_env(fpath):
+    with open(fpath, 'r') as f:
+        line = f.readline()
         while line:
-            keyval=line.split("=")
-            if(len(keyval)==2):
-                os.environ[keyval[0]]=keyval[1]
-            line=f.readline()
+            keyval = line.split("=")
+            if len(keyval) == 2:
+                os.environ[keyval[0]] = keyval[1]
+            line = f.readline()
+
 
 def main():
-    
     if os.environ.get('MINT_XK_CONFIG') is not None and os.path.isfile(os.environ.get('MINT_XK_CONFIG')):
         logger.info("Found an environment XK_CONFIG file for MINT")
-        loadEnv(os.environ.get('MINT_XK_CONFIG'))
-        
-    qApp, args = create_app(sys.argv)
+        load_env(os.environ.get('MINT_XK_CONFIG'))
+
+    q_app, args = create_app(sys.argv)
     dirs.update(__file__)
-    sys.exit(run_app(qApp, args))
+    sys.exit(run_app(q_app, args))
 
 
 if __name__ == "__main__":
