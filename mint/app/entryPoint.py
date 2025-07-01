0492b6df887172572bdb494ac40749b5ef304394
Lana Abadie <lana.abadie@iter.org>
2025-07-01 18:05:16 +0200

    add copyright statement

--- mint/app/entryPoint.py
+++ mint/app/entryPoint.py
@@ -1,3 +1,20 @@
+# Copyright (c) 2020-2025 ITER Organization,
+#               CS 90046
+#               13067 St Paul Lez Durance Cedex
+#               France
+# Author IO
+#
+# This file is part of mint module.
+# mint python module is free software: you can redistribute it and/or modify it under
+# the terms of the MIT license.
+#
+# This file is part of ITER CODAC software.
+# For the terms and conditions of redistribution or use of this software
+# refer to the file LICENSE located in the top level directory
+# of the distribution package
+#
+
+
 # Description: The entry point to MINT application.
 #              All modules are imported only if runApp is actually called. This removes spurious import warnings
 #              when all you run is mint -h to display the usage.
--- mint/models/accessModes/mtPulseId.py
+++ mint/models/accessModes/mtPulseId.py
@@ -1,3 +1,20 @@
+# Copyright (c) 2020-2025 ITER Organization,
+#               CS 90046
+#               13067 St Paul Lez Durance Cedex
+#               France
+# Author IO
+#
+# This file is part of mint module.
+# mint python module is free software: you can redistribute it and/or modify it under
+# the terms of the MIT license.
+#
+# This file is part of ITER CODAC software.
+# For the terms and conditions of redistribution or use of this software
+# refer to the file LICENSE located in the top level directory
+# of the distribution package
+#
+
+
 # Description: Implements a data access mode with pulse id's.
 # Author: Jaswant Sai Panchumarti
 
